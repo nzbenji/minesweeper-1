498a30c14e17941bbe0dacbbf542a665ba2860fb
Ben <benlittle53@gmail.com>
2018-12-13 10:38:03 +1300

    added more game logic and sounds

--- minesweeper.js
+++ minesweeper.js
@@ -2,11 +2,12 @@ document.addEventListener('DOMContentLoaded', startGame)
 
 document.querySelector('.restart').addEventListener('click', restartGame)
 
-// Define your `board` object here!
-var board = {
+let board = {
   cells: []
 }
-const boardSize = 2;
+
+
+var boardSize = 6;
 
 const gameBoard = _ => {
     for( let rows = 0; rows < boardSize; rows++){
@@ -24,11 +25,13 @@ const gameBoard = _ => {
 }
 
 function startGame () {
+  
   gameBoard();
   board.cells.forEach(item => item.surroundingMines = countSurroundingMines(item))
   
   document.addEventListener('click', checkForWin)
   document.addEventListener('contextmenu', checkForWin) //Marks cell as '!'
+  document.addEventListener('click', losingSound)
   
   // Don't remove this function call: it makes the game work!
   lib.initBoard()
@@ -36,23 +39,22 @@ function startGame () {
 
 function restartGame() {
   board = { cells : [] }
-  document.getElementsByClassName('board')[0].innerHTML = '';
+  document.querySelectorAll('.board')[0].innerHTML = '';
+  restartSound();
   startGame();
 }
 
-// Define this function to look for a win condition:
-//
-// 1. Are all of the cells that are NOT mines visible?
-// 2. Are all of the mines marked?
 function checkForWin () {
 
   for(let i = 0; i < board.cells.length; i++) {
-    if(board.cells[i].isMine && board.cells[i].isMarked) return
-    else if(!board.cells[i].isMine && board.cells[i].hidden) return
+    const condition = board.cells[i]
+    if(condition.isMine && condition.isMarked) return
+    else if(!condition.isMine && condition.hidden) return
   }
+  winningSound();
   lib.displayMessage('You win!')
 
-  // board.cells.forEach(check => {
+  // board.cells.forEach((check) => {
   //   if(check.isMine && check.isMarked) return;
   //   else if(!check.isMine && check.hidden) return;
     
@@ -62,15 +64,6 @@ function checkForWin () {
   console.log(board)
 }
 
-
-// Define this function to count the number of mines around the cell
-// (there could be as many as 8). You don't have to get the surrounding
-// cells yourself! Just use `lib.getSurroundingCells`: 
-//
-//   var surrounding = lib.getSurroundingCells(cell.row, cell.col)
-//
-// It will return cell objects in an array. You should loop through 
-// them, counting the number of times `cell.isMine` is true.
 function countSurroundingMines (cell) {
   let surrounding = lib.getSurroundingCells(cell.row, cell.col)
   let count = 0;
@@ -81,5 +74,28 @@ function countSurroundingMines (cell) {
     }
   });
   return count;
+}
+
+const restartSound = _ => {
+  const restart = new Audio('/restartSound.mp3')
+  restart.loop = false;
+  restart.play();
+}
+
+const winningSound = _ => {
+  const winningShaq = new Audio('./winSound.mp3');
+  winningShaq.loop = false;
+  winningShaq.play();
+}
+
+const losingSound = _ => {
+  const losingShaq = new Audio('/bombSound.mp3')
+  losingShaq.loop = false;
+
+  board.cells.forEach((item) => {
+    if(item.isMine && !item.hidden) {
+      losingShaq.play()
+    }
+  })
 }
 
